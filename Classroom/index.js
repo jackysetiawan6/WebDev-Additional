49d73aaa0278868330791c5d70854ebf2b7ec6ee
jackysetiawan6 <jackysetiawan6@gmail.com>
2024-06-14 08:36:49 +0000

    Added classroom seats randomizer.

--- Classroom/index.js
+++ Classroom/index.js
@@ -1,4 +1,4 @@
-const students = [
+let students = [
   "Abdhy",
   "Aryo",
   "Athalia",
@@ -36,5 +36,51 @@ const students = [
   "Ziven",
 ];
 
-const randomStudent = students[Math.floor(Math.random() * students.length)];
-console.log(randomStudent);
+function randomize() {
+  let currentIndex = students.length,
+    randomIndex = 0;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [students[currentIndex], students[randomIndex]] = [
+      students[randomIndex],
+      students[currentIndex],
+    ];
+  }
+}
+
+function setPosition() {
+  let number = 1;
+  desks.forEach((desk) => {
+    if (desk.classList.contains("hide") | desk.classList.contains("skip"))
+      return;
+    desk.textContent = students[number - 1];
+    number++;
+  });
+}
+
+console.log(students);
+
+const button = document.getElementById("start-button");
+const desks = document.querySelectorAll(".desk");
+
+button.addEventListener("click", () => {
+  if (button.classList.contains("active")) return;
+  button.style.backgroundColor = "darkred";
+  button.textContent = "Wait";
+  button.classList.add("active");
+  const afterLoop = () => {
+    button.style.backgroundColor = "green";
+    button.textContent = "Random";
+    button.classList.remove("active");
+  };
+  for (let i = 0; i < 50; i++) {
+    setTimeout(() => {
+      randomize();
+      setPosition();
+      if (i === 49) {
+        afterLoop();
+      }
+    }, i * 50);
+  }
+});
