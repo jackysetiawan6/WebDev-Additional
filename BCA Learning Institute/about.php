c0e0d17be3339c60402d1a08ccc2ea7eb2cf231c
Jacky Setiawan <jackysetiawan6@gmail.com>
2024-07-29 08:16:35 +0700

    Added new BCA Learning Institute website project.

---
+++ BCA Learning Institute/about.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BCA Learning Institute</title>
+    <link rel="shortcut icon" href="assets/favicon-bca.png" type="image/x-icon">
+    <link rel="stylesheet" href="bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-icons-1.11.3/font/bootstrap-icons.css">
+    <style>
+        .primary {
+            color: #0060af;
+        }
+
+        footer {
+            background-color: #031120;
+        }
+
+        #subscribe::placeholder {
+            color: #6f6f6f;
+        }
+    </style>
+</head>
+
+<body>
+    <header class="d-flex px-5 py-3 position-sticky top-0 w-100 bg-white shadow z-3">
+        <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none">
+            <img src="assets/logo-bca-logo-only.png" alt="BCA Learning Institute" style="width: 75px;">
+            <span class="primary fs-4 fw-bold" style="margin-top: -4px;">Learning Institute</span>
+        </a>
+        <ul class="nav gap-3 align-items-center">
+            <li class="nav-item"><a href="booking.php" class="nav-link primary fw-semibold">Bookings</a></li>
+            <li class="nav-item"><a href="about.php" class="nav-link primary fw-semibold"><u>About</u></a></li>
+            <li class="nav-item"><a href="login.php" class="nav-link primary fw-semibold">Login</a></li>
+        </ul>
+    </header>
+    <div class="container-flex">
+        <div class="banner z-n1" style="position: relative; height: 450px; width: 100%; overflow: hidden;">
+            <img src="assets/tower-bca.jpg" class="img w-100 h-100 object-fit-cover" alt="BCA Learning Institute">
+        </div>
+        <div class="w-100 p-5 d-flex flex-column" style="text-align: justify;">
+            <p>Sebagai salah satu instansi keuangan terbesar di Indonesia, PT Bank Central Asia Tbk. (BCA) berkomitmen untuk ikut hadir memberikan dukungan bagi tumbuhnya budaya belajar di perusahaan, khususnya perbankan. Komitmen tersebut ditunjukkan lewat hadirnya BCA Learning Institute, sebuah tempat training center bagi karyawan perusahaan yang hadir lewat wujud bangunan ikonik baru yang berdiri di Sentul, Bogor. Semakin menjadi focal point, karena bangunan ini menerapkan pemilihan warna pada fasadnya yang mengikuti corporate colour, namun tetap senada dengan lingkungan sekitar yang bernuansa hijau dan dikelilingi pepohonan.</p>
+            <p class=" fw-semibold">PEMBAGIAN DUA MASSA BANGUNAN</p>
+            <p>BCA Learning Institute berlokasi di sebuah tanah kosong berkontur dan bertipe clay shale. Perusahaan menginginkan berdirinya sebuah tempat pelatihan yang integratif, sekaligus memiliki suasana yang nyaman untuk sebuah institusi pembelajaran. Selain itu, klien juga menginginkan sebuah tempat pembelajaran yang terbuka dan menjadi “gerbang, portal atau tangga” menuju sisi sosial dan ekonomi yang lebih baik.</p>
+            <p>Sindhu Hadiprana Design Consultant bersama Parametr Architecture menangani sisi arsitektur proyek ini dengan mengusung konsep pembagian dua massa bangunan. Kedua massa tersebut dianalogikan sebagai sebuah portal atau gerbang masuk suatu ruang pembelajaran menuju puncak keberhasilan. Bentuk-bentuk titik keberhasilan dapat hadir lewat communal space atau area bersosialisasi pada ruang amphitheater.</p>
+            <p>Bangunan sebelah utara memiliki 10 lantai, sementara di sisi barat mempunyai 7 lantai. Kedua bangunan tersebut terkoneksi lewat sebuah area pemersatu di lantai dasar dan sebuah gerbang di lantai kedua. Dari massanya sendiri, diterapkan sebuah konsep di mana bangunan ini memberikan space untuk keberadaan breathing room di setiap lantain yang berfungsi sebagai ruang-ruang komunal bangunan dan area lounge.</p>
+            <p class="fw-semibold">ZONING VERTIKAL</p>
+            <p> zoning-nya diterapkan secara vertikal, di mana semakin tinggi lantai, sisi privasinya semakin kuat. Di lantai dasar dibangun ruang publik, seperti lobby, ruang makan, dan BCA Gallery. Sebagai training center, bangunan ini dipenuhi oleh kelas-kelas yang dibagi menurut levelnya, mulai dari kelas standar, eksekutif dan ruang diskusi yang terdiri dari dua buah kelas: indoor dan outdoor (amphitheater). Di bagian atas gedung diletakkan ruang kantor manajemen yang diikuti dengan penempatan roof garden di setiap atap bangunan dan pada jembatan yang menghubungkan pada kedua massanya.</p>
+            <p>Proses pengerjaannya memakan waktu 3 tahun, diawali dengan penyelidikan kondisi tanah pada tahun 2013, di mana hasil kondisi penelitian tersebut memengaruhi rancangan bangunan secara keseluruhan. Kondisi tanah yang bertipe clay shale membuat Sindhu Hadiprana Design Consultant dan Parametr Architecture tidak terlalu banyak mengubah kontur tanah yang ada.</p>
+            <p>Hadirnya BCA Gallery yang berfungsi sebagai area untuk menyimpan informasi sejarah BCA secara interaktif dari awal didirikan hingga saat ini menjadikan sisi edukasi pada bangunan ini terasa sangat kental, tanpa harus melepaskan identitas perusahaan pada desain bangunan utamanya. BCA Learning Institute memang menjadi semacam mini museum yang dapat memberikan gambaran akan sejarah, perjalanan, dan pencapaian perusahaan selama hampir lebih dari 60 tahun dalam melayani kebutuhan perbankan di tanah air. Hal itu secara tepat terwujud lewat ekspresi dan filosofi yang kuat pada desain bangunannya.</p>
+            <div class="w-100 d-grid mb-4" style="grid-template-columns: repeat(auto-fill, minmax(700px, 1fr)); gap: 2rem;">
+                <img src="assets/blueprint-1.jpg" alt="Blueprint BCA Learning Institute" class="img w-100 h-100 object-fit-cover">
+                <img src="assets/blueprint-2.jpg" alt="Blueprint BCA Learning Institute" class="img w-100 h-100 object-fit-cover">
+            </div>
+            <p class=" fw-semibold">Data Proyek</p>
+            <div class="d-flex flex-column gap-2">
+                <p class="mb-0">Nama Proyek: BCA Learning Institute</p>
+                <p class="mb-0">Lokasi: Sentul, Bogor</p>
+                <p class="mb-0">Selesai: 2016</p>
+                <p class="mb-0">Area Pembangunan: 15.159 meter persegi</p>
+                <p class="mb-0">Luas Area Bangunan: 4.441 meter persegi</p>
+                <p class="mb-0">Tinggi Bangunan: 45,6 meter</p>
+                <p class="mb-0">Klien/Pemilik: DPBCA</p>
+                <p class="mb-0">Konsultan Arsitektur: Sindhu Hadiprana Design Consultant & Parametr Architecture</p>
+                <p class="mb-0">Principal Architect: Sindhu Hadiprana, Ario Andito, Harun Wisaksono & Joffi Febriando</p>
+                <p class="mb-0">Konsultan Sipil & Struktur: PT Anugrah Multi Cipta Karya</p>
+                <p class="mb-0">Konsultan Mekanikal & Elektrikal: PT Meltech Consultindo Nusa</p>
+                <p class="mb-0">Geotech Consultant: Geotechnical Engineering Consultant</p>
+                <p class="mb-0">Quantity Surveyor: PT Jurukur Bahan Indonesia</p>
+                <p class="mb-0">Construction Management: PT Global Hospitality Management</p>
+                <p class="mb-0">Foto/Gambar: Lindung Soemarhadi & Ario Andito</p>
+            </div>
+        </div>
+    </div>
+    <footer class="p-5 pb-4">
+        <a href="index.php" class="d-flex align-items-center mb-5 text-decoration-none" style="width: fit-content;">
+            <img src="assets/logo-bca-logo-only-light.svg" alt="BCA Learning Institute" style="width: 70px;">
+            <span class="text-white fs-4 fw-bold ms-1" style="margin-top: -4px;">Learning Institute</span>
+        </a>
+        <div class="d-flex justify-content-between align-items-end flex-wrap">
+            <div class="d-grid text-white" style="grid-template-columns: repeat(3, auto); row-gap: 1rem; column-gap: 5rem;">
+                <p class="fw-bold">Kantor Pusat</p>
+                <p class="fw-bold">Hubungi Kami</p>
+                <p class="fw-bold">Media Sosial</p>
+                <p>Menara BCA, Grand Indonesia</p>
+                <p>
+                    <i class="bi bi-telephone-fill me-2"></i>
+                    Halo BCA 1500888
+                </p>
+                <p>
+                    <i class="bi bi-facebook me-2"></i>
+                    GoodLife BCA
+                </p>
+                <p>Jl. MH Thamrin No. 1 Jakarta Pusat 10310</p>
+                <p>
+                    <i class="bi bi-envelope-fill me-2"></i>
+                    halobca@bca.co.id
+                </p>
+                <p>
+                    <i class="bi bi-instagram me-2"></i>
+                    @goodlifebca
+                </p>
+                <p><a class="text-white" href="https://www.bca.co.id/lokasi-bca">Lokasi BCA Lainnya</a></p>
+                <p>
+                    <i class="bi bi-whatsapp me-2"></i>
+                    +62 811 1500 998
+                </p>
+                <p><a class="text-white" href="https://www.bca.co.id/id/tentang-bca/media-riset/Social-Media">Lihat Semua Media Sosial</a></p>
+            </div>
+            <div class="text-white d-flex flex-column gap-3 mt-5">
+                <p class="m-0">Dapatkan promo dan penawaran terbaik dari BCA</p>
+                <form action="index.php" method="POST" class="d-flex gap-2 align-items-center">
+                    <input type="text" name="subscribe" id="subscribe" class="form-control border-1 shadow-none px-0 border-start-0 border-end-0 border-top-0 rounded-0 bg-transparent text-white" placeholder="Enter your email">
+                    <button class="btn text-white p-0 fs-4"><i class="bi bi-arrow-right"></i></button>
+                </form>
+                <p>This site is protected by reCAPTCHA and the Google<br><a class="text-white" href="https://policies.google.com/privacy">Privacy Policy</a> and <a class="text-white" href="https://policies.google.com/terms">Terms of Service</a> apply.</p>
+            </div>
+        </div>
+        <hr class="text-white mt-5">
+        <div class="d-flex justify-content-between">
+            <div class="d-flex gap-3 text-white flex-wrap">
+                <p>BCA berizin dan diawasi oleh Otoritas Jasa Keuangan & Bank Indonesia</p>
+                <p>BCA merupakan peserta penjaminan LPS</p>
+            </div>
+            <div class="footer-addons d-flex text-white gap-3 flex-wrap">
+                <a class="text-white" href="https://www.bca.co.id/id/informasi/Suku-Bunga-Dasar-Kredit">SBDK</a>
+                <a class="text-white" href="https://www.bca.co.id/id/Syarat-dan-Ketentuan">Kebijakan</a>
+                <a class="text-white" href="https://www.bca.co.id/id/informasi/Kebijakan">Syarat dan Ketentuan</a>
+            </div>
+        </div>
+    </footer>
+    <script src="bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ BCA Learning Institute/detail.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BCA Learning Institute</title>
+    <link rel="shortcut icon" href="assets/favicon-bca.png" type="image/x-icon">
+    <link rel="stylesheet" href="bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-icons-1.11.3/font/bootstrap-icons.css">
+    <style>
+        .primary {
+            color: #0060af;
+        }
+
+        footer {
+            background-color: #031120;
+        }
+
+        #subscribe::placeholder {
+            color: #6f6f6f;
+        }
+
+        .room #room-name {
+            top: 100%;
+            transition: top 0.3s ease-in-out;
+        }
+
+        .room:hover #room-name {
+            top: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <header class="d-flex px-5 py-3 position-sticky top-0 w-100 bg-white shadow z-3" style="height: 85px;">
+        <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none">
+            <img src="assets/logo-bca-logo-only.png" alt="BCA Learning Institute" style="width: 75px;">
+            <span class="primary fs-4 fw-bold" style="margin-top: -4px;">Learning Institute</span>
+        </a>
+        <ul class="nav gap-3 align-items-center">
+            <li class="nav-item"><a href="booking.php" class="nav-link primary fw-semibold">Bookings</a></li>
+            <li class="nav-item"><a href="about.php" class="nav-link primary fw-semibold">About</a></li>
+            <li class="nav-item"><a href="login.php" class="nav-link primary fw-semibold">Login</a></li>
+        </ul>
+    </header>
+    <div class="container-flex">
+        <div class="banner z-n1" style="position: relative; height: 450px; width: 100%; overflow: hidden;">
+            <img src="assets/banner-bca.jpg" class="img w-100 h-100 object-fit-cover" alt="BCA Learning Institute">
+        </div>
+        <div class="search-header bg-white mx-5 px-4 py-3 shadow rounded-3" style="margin-top: -45px;">
+            <form action="search.php" method="GET" class="search w-100" style="display: grid; gap: 1rem; grid-template-columns: 1fr 1fr 1fr 60px;">
+                <div>
+                    <label for="location" class="fw-semibold">Location</label>
+                    <select name="location" id="location" class="form-control border-1 shadow-none px-0 border-start-0 border-end-0 border-top-0 rounded-0">
+                        <option class="text-muted" value="" selected disabled>Select Room</option>
+                        <?php
+                        for ($i = 1; $i <= 20; $i++) {
+                            $number = $i % 2 == 1 ? ($i + 1) * 50 : $i * 50 + 1;
+                            echo "<option value='Discussion $number'>Discussion $number</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div>
+                    <label for="date" class="fw-semibold">Date</label>
+                    <input type="date" name="date" id="date" class="form-control border-1 shadow-none px-0 border-start-0 border-end-0 border-top-0 rounded-0">
+                </div>
+                <div>
+                    <label for="time" class="fw-semibold">Time</label>
+                    <input type="time" name="time" id="time" class="form-control border-1 shadow-none px-0 border-start-0 border-end-0 border-top-0 rounded-0">
+                </div>
+                <button type="submit" class="btn btn-primary rounded-circle border-0" style="width: 60px; height: 60px; align-self: center; background-color: #0060af;">
+                    <i class="bi bi-chevron-right fs-3"></i>
+                </button>
+            </form>
+        </div>
+        <div class="m-5 d-grid" style="grid-template-columns: repeat(auto-fill, minmax(500px, 1fr)); gap: 2rem;">
+            <?php
+            for ($i = 1; $i <= 20; $i++) {
+                $number = $i % 2 == 1 ? ($i + 1) * 50 : $i * 50 + 1;
+                echo "<div class='room shadow rounded-3 overflow-hidden w-100 h-100 position-relative'>";
+                echo "<a href='detail.php' class='text-decoration-none text-white bg-black d-flex justify-content-center align-items-center w-100 h-100 fs-5 mb-0 fw-bold position-absolute opacity-50' id='room-name'>Discussion $number</a>";
+                echo "<img src='assets/room-bca.jpg' class='img w-100 h-100 object-fit-cover' alt='Room'>";
+                echo "</div>";
+            }
+            ?>
+        </div>
+    </div>
+    <footer class="p-5 pb-4">
+        <a href="index.php" class="d-flex align-items-center mb-5 text-decoration-none" style="width: fit-content;">
+            <img src="assets/logo-bca-logo-only-light.svg" alt="BCA Learning Institute" style="width: 70px;">
+            <span class="text-white fs-4 fw-bold ms-1" style="margin-top: -4px;">Learning Institute</span>
+        </a>
+        <div class="d-flex justify-content-between align-items-end flex-wrap">
+            <div class="d-grid text-white" style="grid-template-columns: repeat(3, auto); row-gap: 1rem; column-gap: 5rem;">
+                <p class="fw-bold">Kantor Pusat</p>
+                <p class="fw-bold">Hubungi Kami</p>
+                <p class="fw-bold">Media Sosial</p>
+                <p>Menara BCA, Grand Indonesia</p>
+                <p>
+                    <i class="bi bi-telephone-fill me-2"></i>
+                    Halo BCA 1500888
+                </p>
+                <p>
+                    <i class="bi bi-facebook me-2"></i>
+                    GoodLife BCA
+                </p>
+                <p>Jl. MH Thamrin No. 1 Jakarta Pusat 10310</p>
+                <p>
+                    <i class="bi bi-envelope-fill me-2"></i>
+                    halobca@bca.co.id
+                </p>
+                <p>
+                    <i class="bi bi-instagram me-2"></i>
+                    @goodlifebca
+                </p>
+                <p><a class="text-white" href="https://www.bca.co.id/lokasi-bca">Lokasi BCA Lainnya</a></p>
+                <p>
+                    <i class="bi bi-whatsapp me-2"></i>
+                    +62 811 1500 998
+                </p>
+                <p><a class="text-white" href="https://www.bca.co.id/id/tentang-bca/media-riset/Social-Media">Lihat Semua Media Sosial</a></p>
+            </div>
+            <div class="text-white d-flex flex-column gap-3 mt-5">
+                <p class="m-0">Dapatkan promo dan penawaran terbaik dari BCA</p>
+                <form action="index.php" method="POST" class="d-flex gap-2 align-items-center">
+                    <input type="text" name="subscribe" id="subscribe" class="form-control border-1 shadow-none px-0 border-start-0 border-end-0 border-top-0 rounded-0 bg-transparent text-white" placeholder="Enter your email">
+                    <button class="btn text-white p-0 fs-4"><i class="bi bi-arrow-right"></i></button>
+                </form>
+                <p>This site is protected by reCAPTCHA and the Google<br><a class="text-white" href="https://policies.google.com/privacy">Privacy Policy</a> and <a class="text-white" href="https://policies.google.com/terms">Terms of Service</a> apply.</p>
+            </div>
+        </div>
+        <hr class="text-white mt-5">
+        <div class="d-flex justify-content-between">
+            <div class="d-flex gap-3 text-white flex-wrap">
+                <p>BCA berizin dan diawasi oleh Otoritas Jasa Keuangan & Bank Indonesia</p>
+                <p>BCA merupakan peserta penjaminan LPS</p>
+            </div>
+            <div class="footer-addons d-flex text-white gap-3 flex-wrap">
+                <a class="text-white" href="https://www.bca.co.id/id/informasi/Suku-Bunga-Dasar-Kredit">SBDK</a>
+                <a class="text-white" href="https://www.bca.co.id/id/Syarat-dan-Ketentuan">Kebijakan</a>
+                <a class="text-white" href="https://www.bca.co.id/id/informasi/Kebijakan">Syarat dan Ketentuan</a>
+            </div>
+        </div>
+    </footer>
+    <script src="bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
